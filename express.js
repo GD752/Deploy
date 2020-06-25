dfe93b7dabb0ef2d7eee8ecd2e78a599b2d7337d
Govind <govinddhingra@hotmail.com>
2020-06-26 02:08:05 +0530

    for deployment4

--- express.js
+++ express.js
@@ -23,7 +23,7 @@ socket.broadcast.emit("onend",point)
 // folder designated from which client can get files
 app.use(express.static('public'));
 // server start
-const port=process.env.port||3000
+const port=process.env.PORT||3000
 server.listen(port, function () {
     console.log("Server started at port 3000")
 })
