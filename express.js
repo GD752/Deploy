6c9492b3502d7c5cc3601fea4d7aa273a65a8c19
Govind <govinddhingra@hotmail.com>
2020-06-26 16:59:15 +0530

    for deployment4

--- express.js
+++ express.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+app.use(express.static("public"));
 
 
 
